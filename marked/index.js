0bd032ff96a59528473d3699bcdc8cb8e73b77a5
xuzhijian <zhijian.xu@yuguo.cn>
2017-12-25 14:46:52 +0800

    try marked

--- marked/index.js
+++ marked/index.js
@@ -1,15 +1,13 @@
 /*
  * @Author: Evan 
  * @Date: 2017-12-25 14:41:23 
- * @Last Modified by:   Evan 
- * @Last Modified time: 2017-12-25 14:41:23 
+ * @Last Modified by: Evan
+ * @Last Modified time: 2017-12-25 14:46:44
  */
 const marked = require('marked');
 
-console.log(marked(`I'm using __ MarkDown __`))
+console.log(marked(`I'm using __ MarkDown __`));
 
 var markdownString = '```js\n console.log("hello"); \n```';
 
-console.log(
-  markdownString
-)
+console.log(markdownString);
